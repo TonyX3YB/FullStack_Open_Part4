89b5a1d7dca77eab82b6859f253f05753566bbd2
TonyX3YB <tonyinfo208@gmail.com>
2024-10-02 12:41:57 -0600

    Part 4

--- Part4.15_ble_step3/jest.config.js
+++ Part4.15_ble_step3/jest.config.js
@@ -1,7 +1,7 @@
 export default {
-    transform: {
-      '^.+\\.js$': 'babel-jest',  // Babel is used to transpile ES modules
-    },
-    testEnvironment: 'node',  // Use the Node.js environment for tests
-  };
-  
+  transform: {
+    '^.+\\.js$': 'babel-jest',  // Babel is used to transpile ES modules
+  },
+  testEnvironment: 'node',  // Use the Node.js environment for tests
+  testTimeout: 60000,  // Increase Jest timeout to 20 seconds (20000ms)
+};
