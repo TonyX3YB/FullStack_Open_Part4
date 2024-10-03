b8033789a340b07db201aae53d9506efbe36b477
TonyX3YB <tonyinfo208@gmail.com>
2024-10-03 13:20:59 -0600

    Part 4

--- Part4.18_ble_step6/app.js
+++ Part4.18_ble_step6/app.js
@@ -26,7 +26,9 @@ app.use('/api/login', loginRouter);
 
 
 // Error handling middleware
-const errorHandler = (error, request, response, next)
+const errorHandler = (error, request, response, next) => {
+  
+}
 app.use(unknownEndpoint);
 app.use(errorHandler);
 
