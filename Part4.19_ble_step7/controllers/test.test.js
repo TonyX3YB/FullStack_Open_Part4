2bb24b398cad682828c31f7f450453df5008e016
TonyX3YB <tonyinfo208@gmail.com>
2025-02-01 10:52:50 -0700

    Part 4

--- Part4.19_ble_step7/controllers/test.test.js
+++ Part4.19_ble_step7/controllers/test.test.js
@@ -37,27 +37,5 @@ default:
 epress: 
 extraction: 
 
-const { json } = require("express")
-
-pipeline {
-    agent any 
-
-    envinronment {
-        def myString = "Hello World"
-        def myNumber = 10
-        def myBool = true
-    }
-
-    stages{
-        stage("Demo") {
-            steps {
-                echo "myString: ${myString}"
-                echo "myString: ${myString}"
-                echo "myString: ${myString}"
-
-            }
-        }
-    }
-}
 
 
