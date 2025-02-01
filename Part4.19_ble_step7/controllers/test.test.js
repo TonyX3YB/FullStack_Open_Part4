3f77b3180ae9011949c96bbd0835b87ce61c49ec
TonyX3YB <tonyinfo208@gmail.com>
2025-02-01 10:48:24 -0700

    Part 4

--- Part4.19_ble_step7/controllers/test.test.js
+++ Part4.19_ble_step7/controllers/test.test.js
@@ -1,1 +1,22 @@
+const { json } = require("express")
 
+pipeline {
+    agent any 
+
+    envinronment {
+        def myString = "Hello World"
+        def myNumber = 10
+        def myBool = true
+    }
+
+    stages{
+        stage("Demo") {
+            steps {
+                echo "myString: ${myString}"
+                echo "myString: ${myString}"
+                echo "myString: ${myString}"
+
+            }
+        }
+    }
+}
