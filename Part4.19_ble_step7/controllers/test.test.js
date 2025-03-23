59f71f53d27f84f4dd2d3271d32c41737eba88ec
TonyX3YB <tonyinfo208@gmail.com>
2025-03-23 14:02:04 -0600

    Part 4

--- Part4.19_ble_step7/controllers/test.test.js
+++ Part4.19_ble_step7/controllers/test.test.js
@@ -1,4 +1,47 @@
 
+KeyboardEvent; 
+key: 
+hash: 
+token: 
+port: 
+post: 
+get: 
+del: 
+html: 
+const: 
+await: 
+import: 
+export: 
+return: 
+blog: 
+login: 
+env: 
+root: 
+app: 
+test: 
+json: 
+required: 
+type: 
+schema: 
+string: 
+userschema: 
+deleteModel: 
+returnedobject: 
+mongoose: 
+model: 
+default: 
+express: 
+extraction: 
+
+const { json } = require("express")
+
+pipeline {
+    agent any 
+
+    envinronment {
+        def myString = "Hello World"
+        def myNumber = 10
+        def myBool = true
 
 
 
